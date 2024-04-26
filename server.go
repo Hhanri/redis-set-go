366015b7f61af13dc44e1d44fe529722330218fa
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-26 13:56:55 +0200

    server

---
+++ server.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"net"
+
+	"golang.org/x/exp/slog"
+)
+
+const defaultListenAddr = ":5001"
+
+type Config struct {
+	ListenAddr string
+}
+
+type Server struct {
+	Config
+	peers map[*Peer]bool
+	ln    net.Listener
+
+	addPeerCh chan *Peer
+	quitCh    chan struct{}
+	msgCh     chan []byte
+}
+
+func NewServer(cfg Config) *Server {
+	if cfg.ListenAddr == "" {
+		cfg.ListenAddr = defaultListenAddr
+	}
+	return &Server{
+		Config:    cfg,
+		peers:     make(map[*Peer]bool),
+		addPeerCh: make(chan *Peer),
+		quitCh:    make(chan struct{}),
+		msgCh:     make(chan []byte),
+	}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.ListenAddr)
+	if err != nil {
+		return err
+	}
+	s.ln = ln
+	go s.loop()
+
+	slog.Info("server running", "listenAddr", s.ListenAddr)
+	return s.acceptLoop()
+}
+
+func (s *Server) handleRawMessage(rawMsg []byte) error {
+	cmd, err := parseCommand(string(rawMsg))
+	if err != nil {
+		return err
+	}
+
+	switch v := cmd.(type) {
+	case SetCommand:
+		slog.Info("set key in hashtable", "key", v.key, "value", v.val)
+	}
+	return nil
+}
+
+func (s *Server) loop() {
+	for {
+		select {
+		case rawMsg := <-s.msgCh:
+			if err := s.handleRawMessage(rawMsg); err != nil {
+				slog.Error("raw message error", "err", err)
+			}
+		case <-s.quitCh:
+			return
+		case peer := <-s.addPeerCh:
+			s.peers[peer] = true
+		}
+	}
+}
+
+func (s *Server) acceptLoop() error {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			slog.Error("accept error", "err", err)
+			continue
+		}
+
+		go s.handleConn(conn)
+	}
+}
+
+func (s *Server) handleConn(conn net.Conn) {
+	peer := NewPeer(conn, s.msgCh)
+	s.addPeerCh <- peer
+
+	slog.Info("New peer connected", "remoteAddr", conn.RemoteAddr())
+	if err := peer.readLoop(); err != nil {
+		slog.Error("peer read error", "err", err, "remoteAddr", conn.RemoteAddr())
+	}
+}
