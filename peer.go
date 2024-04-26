5dff0c74a086ceae8da692541472ebc76043baa3
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-26 13:56:38 +0200

    peer

---
+++ peer.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net"
+)
+
+type Peer struct {
+	conn  net.Conn
+	msgCh chan<- []byte
+}
+
+func NewPeer(conn net.Conn, msgCh chan<- []byte) *Peer {
+	return &Peer{
+		conn:  conn,
+		msgCh: msgCh,
+	}
+}
+
+func (p *Peer) readLoop() error {
+	buff := make([]byte, 1024)
+	for {
+		n, err := p.conn.Read(buff)
+		if err != nil {
+			return err
+		}
+
+		msgBuff := make([]byte, n)
+		copy(msgBuff, buff[:n])
+		p.msgCh <- msgBuff
+	}
+}
