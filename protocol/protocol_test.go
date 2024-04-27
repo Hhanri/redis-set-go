0a1fde69170167e0ea930d3e1ececfd9c20a65d9
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-04-27 20:27:33 +0200

    [protocol] fixed TestParseCommand

--- protocol/protocol_test.go
+++ protocol/protocol_test.go
@@ -2,14 +2,23 @@ package protocol
 
 import (
 	"bytes"
+	"io"
 	"testing"
 )
 
 func TestParseCommad(t *testing.T) {
 	msg := "*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n"
 
-	cmd, err := parseCommand(bytes.NewBufferString(msg))
-	if err != nil {
+	var cmd any
+
+	err := parseCommand(
+		bytes.NewBufferString(msg),
+		func(_cmd Command) {
+			cmd = _cmd
+		},
+		func() {},
+	)
+	if err != nil && err != io.EOF {
 		t.Fatal(err)
 	}
 
